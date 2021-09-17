57f71cd73663447ce76842da18447b2d58ce2f6b
Macmaxwell <82811326+Macmaxwell@users.noreply.github.com>
2021-09-17 14:33:44 +0200

    yet to be completed

---
+++ src/components/SearchResults.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const SearchResults = props => {
+  /*return (
+        <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Title</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Surname</th>
+          <th scope="col">Email</th>
+          <th scope="col">Room Id</th>
+          <th scope="col">Check-in</th>
+          <th scope="col">Check-out</th>
+          <th scope="col">Number of Nights</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    )*/
+};
+
+export default SearchResults;
